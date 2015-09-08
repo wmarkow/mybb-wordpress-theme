0d8201edeb1a7da6ce053c1d0728f039a844689c
Witold Markowski <witold.a.markowski@gmail.com>
2015-09-08 23:32:56 +0200

    Add java script to scroll the iframe to a specifi anchor (if needed).

--- wordpress_theme/wordpress_theme.js
+++ wordpress_theme/wordpress_theme.js
@@ -2,6 +2,15 @@ function iframeLoaded() {
 	var iFrameID = document.getElementById('mybb_iframe');
 	if(iFrameID) {
 		iFrameID.height = "";
-		iFrameID.style.height = iFrameID.contentWindow.document.body.scrollHeight + 'px';		
+		iFrameID.style.height = iFrameID.contentWindow.document.body.scrollHeight + 'px';
+
+		var hash = window.location.hash;
+                if(hash) {
+                        var anchorId = hash.replace('#', '');
+                        var anch = iFrameID.contentWindow.document.getElementById(anchorId);
+			if(anch) {        
+				this.scrollTo(0, anch.offsetTop);
+			}
+                }		
 	}
 }
