fb8d5e2fd7ac2f7d4a65dd0e0ea21f45953a7ce8
Witold Markowski <witold.a.markowski@gmail.com>
2015-10-29 18:26:16 +0100

    Set modal window position (on Firefox only).

--- wordpress_theme/wordpress_theme.js
+++ wordpress_theme/wordpress_theme.js
@@ -15,9 +15,17 @@ function iframeLoaded() {
 	}
 }
 
+function intervalChecks() {
+	setIframeSize();
+	setModalPosition();
+}
+
 function setIframeSize() {
-	var ffHeight = document.getElementById("mybb_iframe").contentWindow.document.body.scrollHeight;
-	var chromeHeight = document.getElementById("mybb_iframe").contentDocument.documentElement.scrollHeight;
+	var mybbIframe = document.getElementById("mybb_iframe");
+
+	// set the iframe height on Firefox and others
+	var ffHeight = mybbIframe.contentWindow.document.body.scrollHeight;
+	var chromeHeight = mybbIframe.contentDocument.documentElement.scrollHeight;
 
 	var height = chromeHeight;
 	if (navigator.userAgent.indexOf('Firefox') != -1) {
@@ -25,4 +33,37 @@ function setIframeSize() {
 	}
 
 	document.getElementById("mybb_iframe").style.height = Math.max(ffHeight, chromeHeight) + 'px';
+}
+
+
+function setModalPosition() {
+	var mybbIframe = document.getElementById("mybb_iframe");
+	
+	// set the modal window position on Firefox
+	var currentModals = mybbIframe.contentWindow.document.getElementsByClassName("modal current");
+	if(currentModals.length == 1) {
+		var screenAvailHeight = window.screen.availHeight;
+		var pageYOffset = window.pageYOffset;
+		var mybbIframeOffsetTop = mybbIframe.offsetParent.offsetTop;
+		var mybbIframeHeight = mybbIframe.contentWindow.document.body.scrollHeight;
+		var mozInnerScreenY = window.mozInnerScreenY;
+		var screenY = window.screenY;
+
+		var currentFixedTop = pageYOffset;
+		if(pageYOffset > mybbIframeOffsetTop) {
+			currentFixedTop = pageYOffset -  mybbIframeOffsetTop;
+		}
+
+		var currentFixedBottom = currentFixedTop + screenAvailHeight - mozInnerScreenY - mybbIframeOffsetTop + screenY;
+		if(pageYOffset > mybbIframeOffsetTop){
+			currentFixedBottom = currentFixedBottom + mybbIframeOffsetTop
+		}
+		if(currentFixedBottom > mybbIframeHeight) {
+			currentFixedBottom = mybbIframeHeight;
+		}
+		var currentFixedCenter = (currentFixedTop + currentFixedBottom) / 2;
+
+
+		currentModals[0].style.setProperty ("top", currentFixedCenter + 'px', "important");
+	}
 }
